a572dcb789a99694f07fc066de44bf6a63325a4c
YWXK <1453018126@qq.com>
2020-11-28 18:56:51 +0800

    修复了一些 bug

--- app.js
+++ app.js
@@ -3,15 +3,23 @@ const app = express()
 const port = 8080
 
 // 主页
-app.get('/', (req, res) => {
-    res.sendFile('./views/index.html');
-})
+// app.get('/', (req, res) => {
+//     res.sendFile('./views/index.html');
+// })
 
-// user
-const userRouter = require('./router/router.js');
+// body-parser
+const bodyParser =require('body-parser')
+// post请求解析对象
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('./user', userRouter);
+// user 路由
+const userRouter = require('./router/user.js');
+app.use('/user', userRouter);
 
+// test 静态资源挂载，用于测试
+app.use(express.static('./test'))
+
+// 监听
 app.listen(port, () => {
     console.log(`Home: http://localhost:${port}`)
 })
