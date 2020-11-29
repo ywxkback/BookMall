b2d28a9ce462e848a1cd14a63914d6e6fbb905fc
YWXK <1453018126@qq.com>
2020-11-29 22:21:26 +0800

    Update app.js
    可以解析 post 请求。
    增加 book 路由

--- app.js
+++ app.js
@@ -6,20 +6,20 @@ const port = 8080
 // app.get('/', (req, res) => {
 //     res.sendFile('./views/index.html');
 // })
-
-// body-parser
-const bodyParser =require('body-parser')
-// post请求解析对象
+/* post 请求解析对象 */
+const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended: false}));
-
-// user 路由
+/* user 路由 */
 const userRouter = require('./router/user.js');
 app.use('/user', userRouter);
+/* book 路由 */
+const bookRouter = require('./router/book')
+app.use('/book', bookRouter);
 
-// test 静态资源挂载，用于测试
+/* test 静态资源挂载，用于测试 */
 app.use(express.static('./test'))
 
-// 监听
+/* 监听 */
 app.listen(port, () => {
     console.log(`Home: http://localhost:${port}`)
 })
