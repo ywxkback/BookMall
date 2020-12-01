8c9ff24d8c4b2c0490055c60038888f7c2467fcc
xiebbbbbb <44255689+xiebbbbbb@users.noreply.github.com>
2020-12-01 12:56:09 +0800

    12-1
    13:00

--- app.js
+++ app.js
@@ -3,15 +3,23 @@ const app = express()
 const port = 8080
 
 // 主页
-app.get('/', (req, res) => {
-    res.sendFile('./views/index.html');
-})
+// app.get('/', (req, res) => {
+//     res.sendFile('./views/index.html');
+// })
+/* post 请求解析对象 */
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended: false}));
+/* user 路由 */
+const userRouter = require('./router/user.js');
+app.use('/user', userRouter);
 
-// user
-const userRouter = require('./router/router.js');
+/* test 静态资源挂载，用于测试 */
+app.use(express.static('./test'))
 
-app.use('./user', userRouter);
-
+/* 监听 */
 app.listen(port, () => {
     console.log(`Home: http://localhost:${port}`)
 })
+
+//创建文件app.js,创建web服务器，托管静态资源public目录，包含用户注册的文件，reg.html,点击提交，
+//向服务器发送请求（post  /myreg）  挂载到路由器下面  添加前缀/user
--- router/user.js
+++ router/user.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const pool = require('db/pool');
+const pool = require('./db/pool.js');
 
 const r =express.Router();
 
@@ -11,4 +11,69 @@ const r =express.Router();
 //
 // });
 
-modules.exports = r;
+//用户注册
+r.post('/userRegister',(req,res)=>{
+    //1.获取post 请求数据
+    let obj = req.body;
+    console.log(obj)
+    //2.验证数据是否为空
+    if(!obj.uname){
+        res.send({code:401,msg:'uname required'});
+        //阻止往后执行
+        return;
+    }
+    if(!obj.upwd){
+        res.send({code:402,msg:'upwd required'});
+        //阻止往后执行
+        return;
+    }
+    if(!obj.email){
+        res.send({code:403,msg:'email required'});
+        //阻止往后执行
+        return;
+    }
+    if(!obj.phone){
+        res.send({code:404,msg:'phone required'});
+        //阻止往后执行
+        return;
+    }
+    //执行sql命令  将数据添加到数据库
+    pool.query('INSERT INTO users SET  ?',[obj],(err,result)=>{
+        if(err) throw err;
+        console.log(result);
+        //注册成功
+        res.send({code:200,msg:'reg success'})
+    })
+})
+
+//用户登录
+r.post('/userLogin',(req,res)=>{
+
+    //获取数据
+    let obj = req.body;
+    console.log(obj);
+    //验证数据是否为空
+    if(!obj.uname){
+        res.send({code:401,msg:'uname required'})
+        return;
+    }
+    if(!obj.upwd){
+        res.send({code:402,msg:'upwd required'})
+        return;
+    }
+
+    //到数据库中查询是否有用户名和密码同时匹配的数据
+    pool.query('SELECT * FROM users WHERE uId=? AND uPwd=?',[obj.uname,obj.upwd],(err,result)=>{
+        if(err) throw err;
+        //返回空数组，长度为0 ，说明登录失败
+        if(result.length===0){
+            res.send({code:301,msg:'login err'})
+        }else{//查询到匹配的用户  登录成功
+            res.send({code:200,msg:'login seccess'})
+        }
+        console.log(result);
+    })
+})
+
+//导出路由器
+module.exports = r;
