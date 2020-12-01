2a8ebffa4d0148c28768546a26068be1a17f20fb
YWXK <45416923+ywxkQQc@users.noreply.github.com>
2020-12-01 13:01:29 +0800

    Update app.js

--- app.js
+++ app.js
@@ -7,19 +7,27 @@ const port = 8080
 //     res.sendFile('./views/index.html');
 // })
 
-// body-parser
-const bodyParser =require('body-parser')
-// post请求解析对象
+app.get('/', (req, res) => {
+    res.sendFile('./views/index.html');
+})
+/* post 请求解析对象 */
+const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended: false}));
 
 // user 路由
 const userRouter = require('./router/user.js');
 app.use('/user', userRouter);
 
-// test 静态资源挂载，用于测试
+/* book 路由 */
+const bookRouter = require('./router/book')
+app.use('/book', bookRouter);
+/* cart 路由 */
+const cartRouter = require('./router/cart')
+app.use('/cart', cartRouter);
+/* test 静态资源挂载，用于测试 */
 app.use(express.static('./test'))
 
 // 监听
 app.listen(port, () => {
-    console.log(`Home: http://localhost:${port}`)
+    console.log(`Test: http://localhost:${port}/test.html`)
 })
