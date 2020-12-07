7132ad6d0f04311b876d6b6b41d778e6c06c0115
YWXK <1453018126@qq.com>
2020-12-07 22:26:23 +0800

    Update pool.js

--- db/pool.js
+++ db/pool.js
@@ -3,8 +3,8 @@ const mysql = require('mysql');
 const pool = mysql.createPool({
     host:'127.0.0.1',
     port:'3306',
-    user:'root',                 // 修改为自己的账号的名字
-    password:'12345678',                // 修改为自己的账号的密码
+    user:'lhh',                 // 修改为自己的账号的名字
+    password:'',                // 修改为自己的账号的密码
     database:'bookdb',
     connectionLimit:'20'
 
